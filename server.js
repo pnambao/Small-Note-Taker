f3c88224915dfc7e68733db648a06c27b8b8f724
RoachCoach <panjenambao@gmail.com>
2021-11-18 17:54:36 -0600

    creating routes but something isnt working

--- server.js
+++ server.js
@@ -1,32 +1,53 @@
+//Getting all the packages and stuff we need
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const { uuid } = require('uuid');
+const fs = require("fs");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile);
+const db = require('./db.json');
 
-const PORT = process.env.port || 3001;
-
+// Setting up the express app and the port
 const app = express();
-
-// Import custom middleware, "cLog"
-app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
+const PORT = process.env.PORT || 3005;
+ 
+// Sets up the Express app to handle data parsing (this is the middleware)
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('public'))
+// app.use('/assets', express.static('public/assets'))
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// Routes
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => res.json(db));
+
+// Receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid();
+    db.push(newNote);
+    res.json(newNote);
+    writeFile('db.json', JSON.stringify(db));
+});
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    const chosen = req.params.id;
+    for (note of db) {
+        if (chosen === note.id) {
+            db.splice(note, 1);
+            writeFile('db.json', JSON.stringify(db));
+            res.json(db);
+        };
+    };
+});
+//listens when the server starts running.
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} `)
 );
